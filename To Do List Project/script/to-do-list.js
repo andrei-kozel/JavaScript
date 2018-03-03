8484985cf0f8093b0bd949eec528831d6ed0d5be
elev38 <bexxxa38@gmail.com>
2018-03-03 11:59:22 +0100

    final todo list project

--- To Do List Project/script/to-do-list.js
+++ To Do List Project/script/to-do-list.js
@@ -4,20 +4,25 @@ $('ul').on('click', 'li',  function(){
 });
 
 // click on <span> to delete todo
-$('span').on('click', function(event){
+$('ul').on('click', 'span', function(event){
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
     });
     event.stopPropagation();
 });
 
-//
+// add new todo <li> with input text
 $('input[type="text"]').keypress(function(event){
     if(event.which === 13) {
         // grabbing new todo text from <input>
         var newTodoText = $(this).val();
         $(this).val('');
         // create a new <li> and add to <ul>
-        $('ul').append("<li><span>x</span> " + newTodoText + "</li>");
+        $('ul').append("<li><span><i class='fa fa-trash'></i></span> " + newTodoText + "</li>");
     }
 });
+
+// 
+$('.fa-plus').click(function(){
+    $('input[type="text"]').fadeToggle();
+})
