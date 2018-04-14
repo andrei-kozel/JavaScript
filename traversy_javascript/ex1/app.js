1ecfbbd804ee445f06643ebe0c6a1e648fb90084
elev38 <bexxxa38@gmail.com>
2018-04-14 18:52:41 +0200

    commit

---
+++ traversy_javascript/ex1/app.js
@@ -0,0 +1,22 @@
+// Log to console
+console.log("Hello world");
+console.log(123);
+console.log(true);
+var greeting = "Hello";
+console.log(greeting);
+console.log([1, 2, 3, 4, 5]);
+console.log({ a: 1, b: 2 });
+console.table({ a: 1, b: 2 });
+
+console.error("This is an error");
+console.warn("Warning!");
+console.time("Hello");
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+console.timeEnd("Hello");
---
+++ traversy_javascript/ex2/app.js
@@ -0,0 +1,18 @@
+// var let const
+var name = "John Doe";
+console.log(name);
+
+name = "Steve Smith";
+console.log(name);
+
+const person = {
+  name: "John",
+  age: 30
+};
+
+person.name = "Sarah";
+
+console.log(person);
+
+const sym = Symbol();
+console.log(typeof sym);
